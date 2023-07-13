668d944208f42235186d4f487ecfc647819dc52d
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-07-14 00:33:05 +0300

    trial and error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-nclude <stdio.h>
-
+#include <stdio.h>
 /**
- * * main - program to print value using puts
- * * Description - prints the values on the terminal(screen)
- * * Return: 0 Sucess
- * */
-
+ *  main - program to print value using puts
+ * Description - prints the values on the terminal(screen)
+ * Return: 0 Sucess
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
