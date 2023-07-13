be70989f4f1c553c9a7c78910499828282ecacf9
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-07-13 22:20:08 +0300

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+nclude <stdio.h>
+
+/**
+ * * main - program to print value using puts
+ * * Description - prints the values on the terminal(screen)
+ * * Return: 0 Sucess
+ * */
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
