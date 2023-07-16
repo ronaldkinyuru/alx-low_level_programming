ffac203a916305995f2a1be7ada54dc9523e8eb6
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-07-16 13:13:26 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description -prints the values on the terminal(screen)
+ * Return :Always 0(success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
