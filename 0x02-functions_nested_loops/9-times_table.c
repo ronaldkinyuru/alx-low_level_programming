85d172c1ed32a22c5f2b1560bd227048d9026a08
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-07-19 13:35:13 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,40 +6,34 @@
  *
  * Return: no return
  */
-void times_table(int n)
+void times_table(void)
 {
 	int a, b, op;
 
-	if (n >= 0 && n <= 15)
-	{
-		for (a = 0; a <= n; a++)
+		for (a = 0; a <= 9; a++)
 		{
-			_putchar(48);
-			for (b = 1; b <= n; b++)
+			for (b = 0; b <= 9; b++)
 			{
-				op = a * b;
-				_putchar(44);
-				_putchar(32);
-				if (op <= 9)
+				op  = a * b;
+				if (b == 0)
 				{
-					_putchar(32);
-					_putchar(32);
 					_putchar(op + 48);
 				}
-				else if (op <= 99)
+				if (op < 10 && b != 0)
 				{
 					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(op + 48);
 				}
-				else
+				else if (op >= 10)
 				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
+					_putchar(',');
+					_putchar(' ');
+					_putchar((op % 10) + 48);
 					_putchar((op % 10) + 48);
 				}
 			}
 			_putchar('\n');
-		}
-	}
-}
+			}
+			}
