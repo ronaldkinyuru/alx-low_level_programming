78441bf7c2299a3971b712715dd403b94bef122b
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-07-22 03:12:37 -0700

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,17 @@
 void more_numbers(void)
 {
 	int x, y;
-	
+
 	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
 			if (y > 9)
 			{
-				__putchar((y / 10) + '0');
+			_putchar((y / 10) + '0');
 			}
 			_putchar((y % 10) + '0');
 		}
 		_putchar('\n');
 	}
-
+}
