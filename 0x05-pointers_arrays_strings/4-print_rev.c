a6a5ea359ff11a5e1774aa6622e139017dfa810a
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-07-24 15:08:35 -0700

    I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,18 +6,24 @@
   */
 void print_rev(char *s)
 {
-	int i;
+	int i;/*, j; for for loop*/
 
 	i = 0;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
+	/**
+	* for (j = i - 1; j >= 0; j--)
+	*{
+	*	_putchar(s[j]);
+	*}
+	*/
 	i--;
-	while (i > 0)
+	while (i >= 0)
 	{
 		_putchar(s[i]);
 		i--;
-	}
+	 }
 	_putchar('\n');
 }
