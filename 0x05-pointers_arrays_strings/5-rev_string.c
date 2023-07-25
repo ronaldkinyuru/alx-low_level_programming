a32306ccdceeda5b84a72826d1bfe04fb0bd6ddf
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-07-25 03:15:30 -0700

    hectic

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * rev_string - reverses a string
+  * @s:string to be reversed
+  * Return 0;
+  */
+void rev_string(char *s)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0') /* counts the characters in array */
+	{
+		i++;
+	}
+	i--;
+	j = 0;
+	while(i >= j)
+	{
+		char temp = s[j]; /* Use a temporary variable to swap characters*/
+		s[j] = s[i];
+                s[i] = temp;
+		i--;
+		j++;
+	}
+}
