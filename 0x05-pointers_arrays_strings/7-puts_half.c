7d213b3860fd2656d683802d6f55ce92a4b919b7
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-07-25 07:07:16 -0700

    W7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+  * puts_half - prints second half length of a string
+  *@str: string to be printed
+  * Return: void
+  */
+void puts_half(char *str)
+{
+	int counter, n, j;
+
+	counter = 0;
+	while (str[counter] != '\0') /* counts string chars*/
+	{
+		counter++;
+	}
+	if (counter % 2 != 0) /* checks odd*/
+	{
+		n = (counter - 1) / 2;
+		while (n < counter)
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	else
+	{
+		j = counter / 2;
+		while (j < counter)
+		{
+			_putchar(str[j]);
+			j++;
+		}
+	}
+	_putchar('\n');
+}
