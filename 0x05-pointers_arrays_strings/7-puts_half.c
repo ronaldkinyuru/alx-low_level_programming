774780c2508fe06be47a9654ba4e6e253771a6ba
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-07-25 07:45:28 -0700

    winning

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,25 +6,31 @@
   */
 void puts_half(char *str)
 {
-	int counter, n;
+        int counter, n, j;
 
-	counter = 0;
-	while (str[counter] != '\0') /* counts string chars*/
-	{
-		counter++;
-	}
-	if (counter % 2 == 0) /* checks even*/
-	{
-		n = counter / 2;
-	}
-	else /*odd*/
-	{
-		n = (counter - 1) / 2;
-	}
-	while (str[n] != '\0')
-	{
-			_putchar(str[n]);
+        counter = 0;
+        while (str[counter] != '\0') /* counts string chars*/
+        {
+                counter++;
+        }
+        if (counter % 2 != 0) /* checks odd*/
+        {
+                n = (counter - 1) / 2;
+                while (n < counter)
+                {
+			_putchar(str[n + 1]);
 			n++;
+		}
+	}
+	else
+	{
+		j = counter / 2;
+		while (j < counter)
+		{
+			_putchar(str[j]);
+			j++;
+		}
 	}
 	_putchar('\n');
 }
+
