44ea052c519d645f9b6e1b8bc5c246237e158773
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-08-15 23:33:31 +0300

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog {
+/**
+  * struct dog - new type:struct dog
+  * @name: name of dog
+  * @age: age of the dog
+  * @owner: name of owner of dog
+  * Description: struct with dog details
+  */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
