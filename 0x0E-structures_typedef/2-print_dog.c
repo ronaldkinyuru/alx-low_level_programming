8fed46808485bcbdba9acb5637107666eba79910
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-08-16 01:33:43 +0300

    puppy love

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,6 +13,6 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->owner == NULL)
-		d->owner= "(nil)";
+		d->owner = "(nil)";
 	printf("Name : %s\n Age: %f\n Owner: %s\n",  d->name, d->age, d->owner);
 }
