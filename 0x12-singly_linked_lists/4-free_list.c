2d84a2ae894bcacff372a484885ec69bea028ea2
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-08-24 23:10:28 +0300

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_list -  free a linked list
+ * @head: list_t list to be freed
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+	free_list(head->next);
+	free(head->str);
+	free(head);
+}
