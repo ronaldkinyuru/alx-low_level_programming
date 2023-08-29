d3b1a7eb2f282af40fb7c17c65308291834751e4
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-08-29 14:30:19 +0300

    List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+  * listint_len - prints lenth of node
+  * @h: list
+  * Return: count
+  */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
