68c0ff763ee58b6ae251652d14360a7932f72bcc
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-08-30 13:27:21 +0300

    pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,12 +1,14 @@
 #include "lists.h"
 /**
   * pop_listint - remove head
-  * @head
+  * @head: head list
+  * Return: n value
   */
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
 	int n;
+
 	if (*head == NULL)
 	{
 		return (0);
