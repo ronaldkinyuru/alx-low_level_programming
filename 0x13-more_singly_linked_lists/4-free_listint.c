1850f1e911ce253ed947085b2035c20eec431836
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-08-30 12:13:11 +0300

    free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+  * free_listint - free list
+  * @head:link start
+  * Return:void
+  */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head == NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+	head = NULL;
+}
