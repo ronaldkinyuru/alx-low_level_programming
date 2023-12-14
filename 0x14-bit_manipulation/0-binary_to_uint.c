3ceb0ac529c23fb2bd4b34ad6ee7eb15eda799c7
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-12-14 03:29:40 -0800

    uint to binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to unint
+ * @b: the string to be converted
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+
+}
